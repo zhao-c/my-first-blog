9369afc5246de318c0fbaedc7e104d9efd8ec222
chen zhao <jackchen.ustb@gmail.com>
2018-09-03 12:31:55 +0200

    Add css file, change html file for css decoration, change the post order - the lastest come first

--- blog/views.py
+++ blog/views.py
@@ -9,5 +9,6 @@ from django.utils import timezone
 # CHEN added 02/09/2018
 def post_list(request):
 	# posts var points to the datas from the DB
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	# add - in 'published_date' for posting the lasted post first
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts' : posts})
