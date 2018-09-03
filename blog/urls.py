6a5272f03a955d891ad70617107ddefe7f0e898b
chen zhao <jackchen.ustb@gmail.com>
2018-09-03 16:38:57 +0200

    Added view and template for detailed blog post. Now the templates consist of three: 1. base.html 2. post_list.html 3. post_detail.html Also need add url to blog/usrls.py file.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
+	# CHEN added a url, create a page for a post. 09/03/2018
+	# the name 'post_detail' is equal to the Django template tag added in 'blog/post_list.html'
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,9 @@ from .models import Post
 # for filter
 from django.utils import timezone
 
+# added 09/03/2018
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 # CHEN added 02/09/2018
 def post_list(request):
@@ -12,3 +15,8 @@ def post_list(request):
 	# add - in 'published_date' for posting the lasted post first
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts' : posts})
+
+# added 09/03/2018
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
